3117b2bfd8e18af2acb43014528f07ecc3f407f6
Loveorigami <loveorigami@mail.ru>
2016-10-28 10:24:07 +0300

    migrations

--- README.md
+++ README.md
@@ -18,12 +18,12 @@
 ### 2. Update database schema
 
 ```bash
-$ php yii migrate/up --migrationPath=@vendor/loveorigami/lo-module-email/migrations
-$ php yii migrate/down --migrationPath=@vendor/loveorigami/lo-module-email/migrations
+$ php yii migrate/up --migrationPath=@vendor/loveorigami/lo-module-email/src/migrations
+$ php yii migrate/down --migrationPath=@vendor/loveorigami/lo-module-email/src/migrations
 ```
 
 ### 3. Create database schema
 ```bash
-$ php yii migrate/create --migrationPath=@vendor/loveorigami/lo-module-email/migrations "email_country"
+$ php yii migrate/create --migrationPath=@vendor/loveorigami/lo-module-email/src/migrations "email_country"
 
 ```
---
+++ src/migrations/m161028_071146_email_item.php
@@ -0,0 +1,58 @@
+<?php
+
+use lo\core\db\Migration;
+
+class m161028_071146_email_item extends Migration
+{
+    public $tableGroup = "email";
+
+    const TBL = 'item';
+    const TBL_PARENT = 'cat';
+
+    public function up()
+    {
+        $this->createTable($this->tn(self::TBL), [
+            'id' => $this->primaryKey(),
+            'status' => 'tinyint(1) NOT NULL DEFAULT 0',
+            'author_id' => $this->integer()->notNull(),
+            'updater_id' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+
+            'cat_id' => $this->integer()->notNull(),
+            'session_id' => $this->string(),
+
+            'name' => $this->string(),
+            'email' => $this->string()->notNull(),
+
+            'date_send' => $this->date(),
+            'date_subscribe' => $this->date(),
+            'date_unsubscribe' => $this->date(),
+            'hash' => $this->string(32),
+
+        ]);
+
+        $this->createIndex('idx_email_item_status', $this->tn(self::TBL), 'status');
+        $this->createIndex('idx_email_item_session', $this->tn(self::TBL), 'session_id');
+
+
+    $this->addForeignKey(
+        $this->fk(self::TBL, self::TBL_PARENT),
+        $this->tn(self::TBL), 'cat_id',
+        $this->tn(self::TBL_PARENT), 'id',
+        'CASCADE', 'CASCADE'
+    );
+
+    }
+
+    public function down()
+    {
+        $this->dropTable($this->tn(self::TBL));
+
+        $this->dropForeignKey(
+            $this->fk(self::TBL, self::TBL_PARENT),
+            $this->tn(self::TBL)
+        );
+
+    }
+}
