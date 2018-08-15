76f38cdb04e72cb266afba59ea247298e3b79257
LoveOrigami <loveorigami@mail.ru>
2018-08-15 11:32:59 +0300

    fix migration

--- src/migrations/m170718_124937_email_index.php
+++ src/migrations/m170718_124937_email_index.php
@@ -12,14 +12,14 @@ class m170718_124937_email_index extends Migration
 
     public function up()
     {
-        $this->createIndex('idx_email_item_email', $this->tn(self::TBL), 'email', true);
-        $this->createIndex('idx_email_item_hash', $this->tn(self::TBL), 'hash');
+        $this->createIndex('idx_email_item_email', $this->tn(self::TBL), 'email');
+        //$this->createIndex('idx_email_item_hash', $this->tn(self::TBL), 'hash');
     }
 
     public function down()
     {
         $this->dropIndex('idx_email_item_email', $this->tn(self::TBL));
-        $this->dropIndex('idx_email_item_hash', $this->tn(self::TBL));
+        //$this->dropIndex('idx_email_item_hash', $this->tn(self::TBL));
     }
 
 
